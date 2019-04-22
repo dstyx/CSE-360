2f57b34cf12d3622b1f61471135b5361886537d0
Dean Styx <dean.styx@yahoo.com>
2019-04-21 20:53:17 -0700

    made text area unedible, added button functionality and logic areas

--- cse360Project/src/cse360Project/Frame.java
+++ cse360Project/src/cse360Project/Frame.java
@@ -4,6 +4,7 @@ import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -12,6 +13,9 @@ import javax.swing.JTextArea;
 public class Frame extends JFrame {
 	
 	private Details details;
+	public ArrayList<Item> list = new ArrayList<Item>();
+	
+	
 	public Frame(String title) {
 		super(title);
 		
@@ -21,30 +25,51 @@ public class Frame extends JFrame {
 		//Swing Componenents
 		
 		final JTextArea textArea = new JTextArea();
-		JButton add = new JButton("Insert");
-		
+		textArea.setEditable(false);
 		details = new Details();
+		
 		// Adding componenets to pane
 		Container c = getContentPane();
 		
 		c.add(textArea, BorderLayout.CENTER);
-		//c.add(add,BorderLayout.SOUTH);
 		c.add(details,BorderLayout.WEST);
 		
+		
+		//Detail Listener
 		details.addDetailListener(new DetailListener() {
 			public void detailEventOccured(DetailEvent event) {
 				
-			}
-		});
-		// Behavior
-		add.addActionListener(new ActionListener() {
+				//Func.Add(list, event.getItem() );
+				
+				if(event.getFunc() == 'I') {//INSERT BUTTON LOGIC GOES HERE
+					textArea.append("Added to List\n");
+					String text = Func.itemToString(event.getItem());
+					textArea.append(text);
+				}
+				else if(event.getFunc() == 'D') {//Delete BUTTON LOGIC GOES HERE
+					textArea.append("Deleting entry: \n");
+					String text = Func.itemToString(event.getItem());
+					textArea.append(text);
+				}
+				else if(event.getFunc() == 'R') {//REORDER BUTTON LOGIC GOES HERE
+					textArea.append("Reorganizing List\n");
+				}
+				else if(event.getFunc() == 'U') {//CHANGE BUTTON LOGIC GOES HERE
+					textArea.append("Changing\n");
+				}
+				else if(event.getFunc() == 'P') {//PRINT BUTTON LOGIC GOES HERE
+					textArea.append("Printing\n");
+				}
+				
 
-			@Override
-			public void actionPerformed(ActionEvent arg0) {
-				textArea.append("Hello\n");
 				
 			}
-			
 		});
+		
+		
+
+
 	}
+	
+
 }
