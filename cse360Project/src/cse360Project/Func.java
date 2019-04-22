24d0ac05e0834b1f3f9c5006f88f569bed958f86
Dean Styx <dean.styx@yahoo.com>
2019-04-22 01:00:48 -0700

    Allowed add to actually add to the array list and return it

--- cse360Project/src/cse360Project/Func.java
+++ cse360Project/src/cse360Project/Func.java
@@ -4,12 +4,33 @@ import java.util.ArrayList;
 
 public class Func {
 	
-	public static ArrayList<Item> Add(ArrayList<Item> listIn, Item income) {//List in is old list to be updated, income is incoming item
+	public static ArrayList<Item> Add(ArrayList<Item> listIn, Item incoming) {//List in is old list to be updated, income is incoming item
+		
+		listIn.add(incoming);
+		
+		
+		return listIn;
+	}
+	
+	
+	public static ArrayList<Item> removeItem(ArrayList<Item> listIn, Item incoming){
 		
 		
 		
 		return listIn;
 	}
+	
+	public static ArrayList<Item> sortList(ArrayList<Item> listIn, char sorting){//SORT THE LIST BY CHAR TAHT IS CYCLED THROUGH, P = priority, A = alphabetically, D = by date
+		
+		
+		
+		
+		
+		
+		return listIn;
+	}
+	
+	
 	
 	
 	public static String itemToString(Item item) {
@@ -25,12 +46,13 @@ public class Func {
 		String words = "";
 		
 		if(listIn.isEmpty()) {
-			words = "The To-Do List is empty, please add items";
+			words = "The To-Do List is empty, please add items\n";
 		}
 		else {
 			for(int i = 0; i < listIn.size(); i++) {
 				words += itemToString(listIn.get(i));
 			}
+			words += "\n";
 		}
 		
 		return words;
