1a22db818f5976e80ae1b753961793ff59363540
Dean Styx <dean.styx@yahoo.com>
2019-04-21 20:51:58 -0700

    Added functionality to event, overloaded constructor, get function added

--- cse360Project/src/cse360Project/DetailEvent.java
+++ cse360Project/src/cse360Project/DetailEvent.java
@@ -3,8 +3,32 @@ package cse360Project;
 import java.util.EventObject;
 
 public class DetailEvent extends EventObject {
-	public DetailEvent(Object source) {
+	
+	private Item item;
+	private char func;
+	
+
+	
+	public DetailEvent(Object source, String name, int priority, int month, int day, char status, char func) {//OVERLOADING WITH priority month and day
 		super(source);
+		
+		item = new Item(name, month, day, priority, status);
+		this.func = func;
+		
+	}
+	
+	public DetailEvent(Object source, char func) {
+		super(source);
+		
+		this.func = func;
+	}
+	
+	public Item getItem() {
+		return item;
+	}
+	
+	public char getFunc() {
+		return func;
 	}
 
 }
