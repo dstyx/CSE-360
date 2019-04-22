46b65a0f138cda1e74866f0502f33af56bc6bc0e
Dean Styx <dean.styx@yahoo.com>
2019-04-22 01:00:19 -0700

    Created a functioning adding method, added clear logic area

--- cse360Project/src/cse360Project/Frame.java
+++ cse360Project/src/cse360Project/Frame.java
@@ -8,11 +8,13 @@ import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
 public class Frame extends JFrame {
 	
 	private Details details;
+	private char sort = 'P';
 	public ArrayList<Item> list = new ArrayList<Item>();
 	
 	
@@ -25,13 +27,14 @@ public class Frame extends JFrame {
 		//Swing Componenents
 		
 		final JTextArea textArea = new JTextArea();
+		final JScrollPane scroll = new JScrollPane(textArea);
 		textArea.setEditable(false);
 		details = new Details();
 		
 		// Adding componenets to pane
 		Container c = getContentPane();
 		
-		c.add(textArea, BorderLayout.CENTER);
+		c.add(scroll, BorderLayout.CENTER);
 		c.add(details,BorderLayout.WEST);
 		
 		
@@ -45,6 +48,7 @@ public class Frame extends JFrame {
 					textArea.append("Added to List\n");
 					String text = Func.itemToString(event.getItem());
 					textArea.append(text);
+					Func.Add(list, event.getItem());
 				}
 				else if(event.getFunc() == 'D') {//Delete BUTTON LOGIC GOES HERE
 					textArea.append("Deleting entry: \n");
@@ -52,13 +56,34 @@ public class Frame extends JFrame {
 					textArea.append(text);
 				}
 				else if(event.getFunc() == 'R') {//REORDER BUTTON LOGIC GOES HERE
-					textArea.append("Reorganizing List\n");
+					if(sort == 'P') {
+						sort = 'A';
+						textArea.append("Now sorting alphabetically");
+					}
+					else if(sort == 'A') {
+						sort = 'D';
+						textArea.append("Now sorting by due date");
+
+					}
+					else {
+						sort = 'P';
+						textArea.append("Now sorting by priority");
+					}
+					textArea.append("Reorganizing List\n" + sort);
 				}
 				else if(event.getFunc() == 'U') {//CHANGE BUTTON LOGIC GOES HERE
 					textArea.append("Changing\n");
 				}
 				else if(event.getFunc() == 'P') {//PRINT BUTTON LOGIC GOES HERE
+					String text = Func.listToString(list);
 					textArea.append("Printing\n");
+					textArea.append(text);
+				}
+				else if(event.getFunc() == 'C') {//CLEAR BUTTON LOGIC
+					
+				}
+				else if(event.getFunc() == 'E') {//ERROR LOGIC GOES HERE
+					textArea.append("ERROR, check user guide for inputs");
 				}
 				
 
